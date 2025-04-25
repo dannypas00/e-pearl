2fce12b6df874cf7215e01ecbbaaee5b00cea9eb
dannypas00 <github@dannypas.nl>
2025-04-25 12:30:40 +0200

    Fix tailwind, prettier config, and add check and fix npm scripts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/test-utils'
-  ]
-})
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image', '@nuxt/test-utils'],
+
+  css: ['~/assets/css/tailwind.css'],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
